75f9673b6c50ef467dfcc038fe91a1f6b6c27ede
williammasivi <williammasivi@gmail.com>
2024-05-05 14:20:37 +0200

    starter-code

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-extrabold m-2 text-center">
+        DYNAMIC SITE CHALLENGE NETLIFY STARTER CODE
+      </h1>
+    </>
+  );
+};
+
+export default App;
