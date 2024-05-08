f5f2b98f120b044615c6fe8eb45dafa2d0d83a68
williammasivi <williammasivi@gmail.com>
2024-05-08 10:15:36 +0200

    header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import React from "react";
+import { BrowserRouter, Router, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import CrewPage from "./pages/CrewPage";
+import DestinationPage from "./pages/DestinationPage";
+import TechnologyPage from "./pages/TechnologyPage";
 
 const App = () => {
   return (
-    <>
-      <HomePage />
-    </>
+    <Router>
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/crew" component={CrewPage} />
+      <Route exact path="/destination" component={DestinationPage} />
+      <Route exact path="/technology" component={TechnologyPage} />
+    </Router>
   );
 };
 
--- src/routes/LayoutRoute.jsx
+++ src/routes/LayoutRoute.jsx
@@ -1,11 +1,11 @@
-import { Outlet } from 'react-router-dom';
-
+import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
 const LayoutRoute = () => {
-    return (
-        <>
-            <h1>Outlet Element</h1>
-        </>
-    );
+  return (
+    <>
+      <h1>Outlet Element</h1>
+    </>
+  );
 };
 
 export default LayoutRoute;
