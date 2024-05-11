11b3a8c592de635e9c1556e957ab0b29bb092978
williammasivi <williammasivi@gmail.com>
2024-05-11 11:58:28 +0200

    destination page

--- src/pages/CrewPage.jsx
+++ src/pages/CrewPage.jsx
@@ -1,23 +1,26 @@
 const CrewPage = () => {
   return (
-    <>
+    <div className="flex flex-col items-center justify-center h-scree ">
       <h1>Crew Page</h1>
-      <h3>02 MEET YOUR CREW</h3>
-      <h3>COMMANDER</h3>
-      <h1>DOUGLAS HURLEY</h1>
-      <p>
+      <h1 className="">02 MEET YOUR CREW</h1>
+      <h1 className="absolute w-214 h-37 left-165.5 top-400 font-Bellefair font-normal text-white text-2xl leading-37 uppercase mix-blend-normal opacity-50">
+        COMMANDER
+      </h1>
+      <h1 className="absolute w-488 h-64 left-165.5 top-452 font-Bellefair font-normal text-white text-4xl leading-64 uppercase">
+        DOUGLAS HURLEY
+      </h1>
+      <p className="absolute w-444 h-128 left-165 top-543 text-white">
         Douglas Gerald Hurley is an American engineer, former <br /> Marine
         Corps pilot and former NASA astronaut. He <br /> launched into space for
         the third time as commander of <br /> Crew Dragon Demo-2.
       </p>
       <img src="/background-crew-desktop.jpg" alt="" />
       <div style={{ backgroundImage: "/background-crew-desktop.png" }}></div>
-      <img
-        className="absolute top-1/2 left-1/2 justify-center items-center w-568 h-702 left-735 top-198"
-        src="/image-removebg-preview(289).png"
-        alt=""
-      />
-    </>
+
+      <div className="absolute w-445 h-445 left-179 top-324 left-12 right-0 top-1/2">
+        <img className="" src="/image-removebg-preview(289).png" alt="" />
+      </div>
+    </div>
   );
 };
 
--- src/pages/DestinationPage.jsx
+++ src/pages/DestinationPage.jsx
@@ -1,24 +1,35 @@
 const DestinationPage = () => {
   return (
-    <>
-      <h1>Destination Page</h1>
-      <h3>01 PICK YOUR DESTINATION</h3>
-      <h1>MOOM</h1>
-      <p>
-        {" "}
-        See our planet as you’ve never seen it before. A perfect <br /> relaxing
-        trip away to help regain perspective and come <br /> back refreshed.
-        While you’re there, take in some history <br /> by visiting the Luna 2
-        and Apollo 11 landing sites.{" "}
-      </p>
-      <img src="/background-crew-desktop.jpg" alt="" />
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="">Destination Page</h1>
+
+      <div className="absolute w-382 h-34 left-166.5 top-212  text-white">
+        01 PICK YOUR DESTINATION
+      </div>
+
+      <div className="grid grid-cols-1">
+        <div className="absolute h-115 left-0.11 right-[25.06%] top-[calc(50% - 115px/2 - 107.5px)] font-Bellefair font-normal text-white text-6xl leading-[115px]">
+          MOOM
+        </div>
+
+        <div className="absolute h-32 left-0 right-0 top-1/2 transform -translate-y-1/2">
+          <p className="font-barlow font-normal text-base leading-8 text-white">
+            See our planet as you’ve never seen it before. A perfect <br />{" "}
+            relaxing trip away to help regain perspective and come <br /> back
+            refreshed. While you’re there, take in some history <br /> by
+            visiting the Luna 2 and Apollo 11 landing sites.{" "}
+          </p>
+        </div>
+      </div>
+
+      <img className="" src="/background-crew-desktop.jpg" alt="" />
       <div style={{ backgroundImage: "/background-crew-desktop.png" }}></div>
       <img
-        className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+        className="absolute w-445 h-445 left-179 top-324 left-12 right-0 top-1/2"
         src="/image-moon.png"
         alt=""
       />
-    </>
+    </div>
   );
 };
 
